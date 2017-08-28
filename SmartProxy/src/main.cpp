15fad82c1235a41dca39047eb2053c7d8b76067d
Michael <1985082200@qq.com>
2017-08-28 17:02:04 +0800

    fix bug: add CROW_MSVC_WORKAROUND to compile on windows

--- SmartProxy/src/main.cpp
+++ SmartProxy/src/main.cpp
@@ -8,10 +8,16 @@ int main()
 {
 	crow::SimpleApp app;
 
-	CROW_ROUTE(app, "/")(process_route_request);
-	CROW_ROUTE(app, "/CameraOcclusionServiceRequest").methods("POST"_method)(process_camera_occlusion_service_request);
-	CROW_ROUTE(app, "/RegisterService").methods("POST"_method)(process_register_service_request);
-	CROW_ROUTE(app, "/UnRegisterService").methods("POST"_method)(process_unregister_occlusion_service_request);
+	app.route_dynamic("/")([] {
+		return "Hello World!";
+	});
+
+	//app.route_dynamic("/")(&process_route_request);
+
+	//CROW_ROUTE(app, "/")(process_route_request);
+	//CROW_ROUTE(app, "/CameraOcclusionServiceRequest").methods(crow::HTTPMethod::Post)(process_camera_occlusion_service_request);
+	//CROW_ROUTE(app, "/RegisterService").methods(crow::HTTPMethod::Post)(process_register_service_request);
+	//CROW_ROUTE(app, "/UnRegisterService").methods(crow::HTTPMethod::Post)(process_unregister_occlusion_service_request);
 
 	app.port(18080).multithreaded().run();
 }
